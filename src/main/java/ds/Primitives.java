e1bf0ebacf95e5dc17f8c3e3c5f200785b774c90
hardikmahant <hthinksmart@gmail.com>
2018-05-06 22:45:19 -0700

    LeetCode Project kickoff

---
+++ src/main/java/ds/Primitives.java
@@ -0,0 +1,7 @@
+package ds;
+
+public class Primitives {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/lc/TwoSum.java
@@ -0,0 +1,7 @@
+package lc;
+
+public class TwoSum {
+    public static void main(String[] args) {
+
+    }
+}
